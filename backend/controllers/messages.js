b0229ece97745bef8f20730a8236939d0681d70e
Aaro Paltemaa <apaltemaa@gmail.com>
2025-05-20 11:49:40 +0300

    update readme file

--- backend/controllers/messages.js
+++ backend/controllers/messages.js
@@ -1,5 +1,6 @@
 const messagesRouter = require("express").Router();
 const Message = require("../models/message");
+const User = require("../models/user");
 const middleware = require("../utils/middleware");
 
 messagesRouter.post("/", middleware.userExtractor, async (req, res) => {
@@ -22,6 +23,44 @@ messagesRouter.post("/", middleware.userExtractor, async (req, res) => {
   res.status(201).json(savedMessage);
 });
 
+messagesRouter.get("/threads", middleware.userExtractor, async (req, res) => {
+  const loggedInUser = req.user._id;
+
+  const messages = await Message.find({
+    $or: [{ sender: loggedInUser }, { receiver: loggedInUser }],
+  }).sort({ timestamp: -1 });
+
+  const conversations = {};
+
+  for (const message of messages) {
+    const senderId = message.sender.toString();
+    const receiverId = message.receiver.toString();
+
+    const otherUserId =
+      senderId === loggedInUser.toString() ? receiverId : senderId;
+
+    if (!conversations[otherUserId]) {
+      conversations[otherUserId] = message;
+    }
+  }
+
+  const otherUserIds = Object.keys(conversations);
+
+  const users = await User.find({ _id: { $in: otherUserIds } }).select(
+    "name email skillsOffered skillsWanted"
+  );
+
+  const threads = users.map((user) => {
+    const lastMessage = conversations[user._id.toString()];
+    return {
+      user,
+      lastMessage,
+    };
+  });
+
+  res.json(threads);
+});
+
 messagesRouter.get("/:userId", middleware.userExtractor, async (req, res) => {
   const loggedInUser = req.user._id;
   const otherUser = req.params.userId;
@@ -31,7 +70,7 @@ messagesRouter.get("/:userId", middleware.userExtractor, async (req, res) => {
       { sender: loggedInUser, receiver: otherUser },
       { sender: otherUser, receiver: loggedInUser },
     ],
-  });
+  }).sort({ timestamp: 1 });
   res.json(messages);
 });
 
