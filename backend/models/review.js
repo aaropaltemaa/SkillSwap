88277bd22eb1a7f35e8591320f06b95ddd4ea8b5
Aaro Paltemaa <apaltemaa@gmail.com>
2025-07-01 11:03:32 +0300

    created review schema for fulfilled exchange requests

---
+++ backend/models/review.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    reviewer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reviewee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    exchange: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ExchangeRequest",
+      required: false,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      maxlength: 1000,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+);
+
+reviewSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
