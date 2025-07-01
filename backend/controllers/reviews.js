1607935a3d2ceff9b42012934e22384a708c5261
Aaro Paltemaa <apaltemaa@gmail.com>
2025-07-01 11:50:13 +0300

    added get routes for reviews

--- backend/controllers/reviews.js
+++ backend/controllers/reviews.js
@@ -4,7 +4,6 @@ const Review = require("../models/review");
 const User = require("../models/user");
 const ExchangeRequest = require("../models/exchangerequest");
 
-// POST /api/reviews
 reviewsRouter.post(
   "/",
   middleware.userExtractor,
@@ -53,6 +52,54 @@ reviewsRouter.post(
 
       const savedReview = await newReview.save();
       res.status(201).json(savedReview);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Get all reviews for a user (as reviewee)
+reviewsRouter.get(
+  "/user/:userId",
+  async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      const reviews = await Review.find({ reviewee: userId })
+        .populate("reviewer", "username name")
+        .populate("exchange");
+      res.json(reviews);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Get all reviews written by a user (as reviewer)
+reviewsRouter.get(
+  "/reviewer/:userId",
+  async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      const reviews = await Review.find({ reviewer: userId })
+        .populate("reviewee", "username name")
+        .populate("exchange");
+      res.json(reviews);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Get all reviews for a specific exchange
+reviewsRouter.get(
+  "/exchange/:exchangeId",
+  async (req, res, next) => {
+    try {
+      const { exchangeId } = req.params;
+      const reviews = await Review.find({ exchange: exchangeId })
+        .populate("reviewer", "username name")
+        .populate("reviewee", "username name");
+      res.json(reviews);
     } catch (error) {
       next(error);
     }
