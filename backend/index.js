b0a8029863f97d2cb64df0a217ee1d3c60176149
Aaro Paltemaa <apaltemaa@gmail.com>
2025-07-03 21:51:05 +0300

    installed socket io

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,29 @@
 const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const http = require("http");
+const { Server } = require("socket.io");
 
-app.listen(config.PORT, () => {
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST"],
+  },
+});
+
+// Socket.IO event handling
+io.on("connection", (socket) => {
+  logger.info("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    logger.info("User disconnected:", socket.id);
+  });
+});
+
+app.set("io", io);
+
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
